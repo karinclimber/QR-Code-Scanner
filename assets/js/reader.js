f283bfa69c76a4644da6fb7d0714b8ec8e7f12cf
Karin Hermann <karinclimber@gmail.com>
2017-09-28 20:00:39 -0400

    js

--- assets/js/reader.js
+++ assets/js/reader.js
@@ -1,23 +1,31 @@
-$("#gobutton").click(function() {
-  $(location).attr('href','#userInput');
-});
+$(document).ready(function() {
+  $("#gobutton").click(function() {
+    var userInput = $("#url").val();
+    console.log(userInput);
+   // window.location(userInput)
+    location.href = userInput;
+    //$(location).attr("href", "userInput");
+  });
 
- function openQRCamera(node) {
-  var reader = new FileReader();
-  reader.onload = function() {
-    node.value = "";
-    qrcode.callback = function(res) {
-      if(res instanceof Error) {
-        alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
-      } else {
-        node.parentNode.previousElementSibling.value = res;
-      }
+  function showQRIntro() {
+    return confirm("Use your camera to take a picture of a QR code.");
+  }
+
+  function openQRCamera(node) {
+    var reader = new FileReader();
+    reader.onload = function() {
+      node.value = "";
+      qrcode.callback = function(res) {
+        if (res instanceof Error) {
+          alert(
+            "No QR code found. Please make sure the QR code is within the camera's frame and try again."
+          );
+        } else {
+          node.parentNode.previousElementSibling.value = res;
+        }
+      };
+      qrcode.decode(reader.result);
     };
-    qrcode.decode(reader.result);
-  };
-  reader.readAsDataURL(node.files[0]);
-}
-
-function showQRIntro() {
-  return confirm("Use your camera to take a picture of a QR code.");
-}
+    reader.readAsDataURL(node.files[0]);
+  }
+});
